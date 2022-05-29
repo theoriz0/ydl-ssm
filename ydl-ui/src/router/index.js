9d585f2cd6e3b4db0bfd0d3a1061ec5f6a7750fb
theoriz0 <zhang_00710@sina.com>
2022-05-29 23:53:08 +0800

    update login, main router

--- ydl-ui/src/router/index.js
+++ ydl-ui/src/router/index.js
@@ -8,7 +8,16 @@ import {
  * 定义路由信息
  * 
  */
-const routes = [];
+const routes = [{
+    name: 'login',
+    path: '/login',
+    component: () => import('@/components/login')
+},{
+    name: 'main',
+    alias: '/',
+    path: '/main',
+    component: () => import('@/components/main')
+}];
 
 // 创建路由实例并传递 `routes` 配置
 // 我们在这里使用 html5 的路由模式，url中不带有#，部署项目的时候需要注意。
@@ -20,8 +29,7 @@ const router = createRouter({
 
 // 全局的路由守卫
 router.beforeEach((to, from) => {
-    console.log(to);
-    console.log(from);
+    //每次进行路由切换都判断一下有没有登陆，如果没有登陆，跳转到登陆页
     return true;
 })
 
