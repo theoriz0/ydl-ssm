eeb0cb86d3d43128b1f34260c5542fdec0dc1ab5
theoriz0 <zhang_00710@sina.com>
2022-05-31 23:38:06 +0800

    Feat: intercept requests to add auth header

--- ydl-ui/src/api/index.js
+++ ydl-ui/src/api/index.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 10000,
     headers: {'Content-Type': 'application/json;charset=utf-8'}
 })
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+    if (store.state.user.token) {
+        config.headers['Authorization'] = store.state.user.token
+    }
+    // Do something before request is sent
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
 export default request
