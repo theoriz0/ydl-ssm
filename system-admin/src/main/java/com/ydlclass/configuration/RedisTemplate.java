efe674b28706e52eea1d8911bc1fb05775d2f11d
theoriz0 <zhang_00710@sina.com>
2022-05-31 19:12:04 +0800

    add and test redis service

---
+++ system-admin/src/main/java/com/ydlclass/configuration/RedisTemplate.java
@@ -0,0 +1,83 @@
+package com.ydlclass.configuration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisException;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+public class RedisTemplate {
+    @Resource
+    private JedisPool jedisPool;
+
+    @Resource
+    private CustomObjectMapper objectMapper;
+
+    //保存字符串类型数据
+    public String set(String key, String value, Long expire) {
+        Jedis resource = jedisPool.getResource();
+        String returnValue = null;
+        try {
+            if (expire > 0) {
+                returnValue = resource.setex(key, expire, value);
+            } else {
+                returnValue = resource.set(key, value);
+            }
+        } catch (JedisException jedisException) {
+            log.error("redis execution error!", jedisException);
+            jedisPool.returnBrokenResource(resource);
+        } finally {
+            jedisPool.returnResource(resource);
+        }
+        return returnValue;
+    }
+
+    //获取字符串类型数据
+    public String get(String key) {
+        Jedis resource = jedisPool.getResource();
+        String returnValue = null;
+        try {
+            returnValue = resource.get(key);
+        } catch (JedisException jedisException) {
+            log.error("redis execution error!", jedisException);
+            jedisPool.returnBrokenResource(resource);
+        } finally {
+            jedisPool.returnResource(resource);
+        }
+        return returnValue;
+    }
+
+    //存储序列化数据
+    public String setObject(String key, Object value, Long expire) {
+        String returnValue = null;
+        try {
+            String jsonValue = objectMapper.writeValueAsString(value);
+            returnValue = set(key, jsonValue, expire);
+        } catch (JsonProcessingException exception) {
+            log.error("redis execution error!", exception);
+        }
+        return returnValue;
+    }
+
+    //获取序列化数据
+    public <T> T getObject(String key, TypeReference<T> typeReference) {
+        Jedis resource = jedisPool.getResource();
+        T returnValue = null;
+        try {
+            String valueString = resource.get(key);
+            returnValue = objectMapper.readValue(valueString, typeReference);
+        } catch (JedisException | JsonProcessingException e) {
+            log.error("redis execution error!", e);
+            jedisPool.returnBrokenResource(resource);
+        } finally {
+            jedisPool.returnResource(resource);
+        }
+        return returnValue;
+    }
+}
--- system-admin/src/main/java/com/ydlclass/controller/TestController.java
+++ system-admin/src/main/java/com/ydlclass/controller/TestController.java
@@ -1,18 +1,33 @@
 package com.ydlclass.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.ydlclass.configuration.RedisTemplate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+
 @Controller
+@Slf4j
 public class TestController{
-    @GetMapping("user")
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("test")
     @ResponseBody
-    public User test() {
-        return new User("tom", 12);
+    public String test() {
+        redisTemplate.setObject("map", List.of("zs", "lisi", "ww"), -1L);
+        List<String> list = redisTemplate.getObject("map", new TypeReference<>(){});
+        log.info(list.toString());
+        return "hello-ssm";
     }
 
     @Getter
